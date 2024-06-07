4f39a78a20c2aa8d4446ec779a16a48706569762
Lucca Maia <luccamaiarosa@gmail.com>
2024-06-07 18:02:03 -0300

    javascript e modal adicionados

---
+++ WebDev/html/index.js
@@ -0,0 +1,10 @@
+//Funções para abrir um pop-up onde colocarmos botões onclick
+function abrirModal() {
+    let modal = document.getElementById('modal');
+    modal.style.display = 'block';
+}
+
+function fecharModal() {
+    let modal = document.getElementById('modal');
+    modal.style.display = 'none';
+}
