986ce4ed593bdb0961b7c7dbe72110dd45ea1c84
Lucca Maia <luccamaiarosa@gmail.com>
2024-06-12 23:53:57 -0300

    Ajustes index, dadosatualizados, modais (css,html)

--- WebDev/html/index.js
+++ WebDev/html/index.js
@@ -1,12 +1,20 @@
 //Funções para abrir um pop-up onde colocarmos botões onclick
 function abrirModal() {
   let modal = document.getElementById('modal');
-  modal.style.display = 'block';
+  let areaModal = document.getElementById('areaModal');
+  modal.classList.add('show');
+  setTimeout(() => {
+    areaModal.classList.add('open');
+  }, 10); // Timeout para permitir que o navegador aplique a mudança de estilo
 }
 
 function fecharModal() {
   let modal = document.getElementById('modal');
-  modal.style.display = 'none';
+  let areaModal = document.getElementById('areaModal');
+  areaModal.classList.remove('open');
+  setTimeout(() => {
+    modal.classList.remove('show');
+  }, 500); // Tempo da animação para ocultar o modal
 }
 
 $(document).ready(function () {
